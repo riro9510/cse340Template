0074365d8dcd898b888b13991f9f0167ad56e727
riro9510 <riro9510@gmail.com>
2025-09-15 06:46:13 -0600

    activities week 3

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 /* ***********************
  * View Engine and templates
  *************************/
@@ -24,10 +25,10 @@ app.set("layout", "./layouts/layout")
  * Routes
  *************************/
 app.use(static)
-app.get("/", function (req, res) {
-  res.render("index", {
-    title: "Home",
-  })
+app.get("/", utilities.handleErrors(baseController.buildHome))
+app.use("/inv", inventoryRoute)
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 
 /* ***********************
@@ -40,6 +41,19 @@ const host = process.env.HOST
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
